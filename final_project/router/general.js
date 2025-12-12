80bc76bf72e692f46b1bc20edcf7d8d5f7990999
Viktorija Sprainyte <viktapikta@gmail.com>
2025-12-12 06:10:48 -0500

    Task2: return book by ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,7 +18,13 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+  
+  if (book == undefined)
+    res.send(404);
+
+  res.send(book);
  });
   
 // Get book details based on author
